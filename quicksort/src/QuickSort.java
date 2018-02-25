063c38af8f243d913e668d7ca3bd7750fdc9d746
FANG CAO <fc63@njit.edu>
2018-02-25 12:28:03 -0500

    quicksort added and working

--- quicksort/src/QuickSort.java
+++ quicksort/src/QuickSort.java
@@ -1,51 +1,88 @@
 
-public class QuickSort {
+public class QuickSort 
+{
+	private int array[];
 	
 	public QuickSort()
 	{
 		
 	}
+	
+    public void QuickSort(int[] a) {
+        // check for empty or null array
+        if (a ==null || a.length==0){
+            return;
+        }
+        
+        this.array = a;
+        Sort(array, 0, a.length - 1);
+    }
+    
 	public void Sort(int a[], int leftIdx, int rightIdx)
 	{
 		if(leftIdx >= rightIdx)
 			return;
-				
-		int piviotIdx = getPiviot(a, leftIdx, rightIdx);
-		Swap(a, a[leftIdx], a[piviotIdx]);
 		
-		int piviotVal = a[piviotIdx];
+		// left to right 
+		int l = leftIdx;	// compared left element
+		int r = rightIdx;	// compared right element
+		int piviotVal = getPiviotIdx(a,l,r);		
+
+		while(l<=r)
+		{
+			while(a[l] < piviotVal) 
+				l++;
+			
+			while(a[r] > piviotVal) 
+				r--;
+			
+			if(l <= r)
+			{
+				Swap(a,l,r);
+				l++;
+				r--;
+			}
+			
+		}
 		
-		
+		if(leftIdx < r)
+			Sort(a,leftIdx,r);
+		if(rightIdx > l)
+			Sort(a,l,rightIdx);
 	}
 	
-	private int getPiviot(int a[], int lBound, int rBound)
+	private int getPiviotIdx(int a[], int l, int r)
 	{
-		// pick the middle point as piviot for now
-		int piviot;
-		piviot = a[(lBound+rBound)/2];
-		
-		return piviot;
+		// pick the middle point as piviot for now		
+		return a[l + (r-l)/2];
 	}
 	
-	public void Swap(int array[], int a, int b)
+	public void Swap(int a[], int i, int j)
+	{	
+		int tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+	}
+	
+	public void PrintArray(int a[])
 	{
-		
+		for(int i=0; i<a.length; i++)
+			System.out.printf("%d, ",a[i]);
+		System.out.println();
 	}
 	
 	public static void main(String[] args) {
+		
 		int array[] = new int[32];
-		int size  = 32; 
+		for(int i=0; i<array.length; i++)
+			array[i] = array.length - i;
 		
-		// fill the 32 elements array in desending order
-		for(int i=0; i<size; i++)		
-			array[i] =  size - 1 - i;
-			
-		for(int i=0; i<size; i++)
-			System.out.printf("array[%d] = %d\n", i,array[i]);
 		
-		QuickSort qs = new QuickSort();
-		qs.Sort();
+		QuickSort qs = new QuickSort();	
 		
+		qs.PrintArray(array);				
+		qs.Sort(array, 0, array.length-1);		
+		qs.PrintArray(array);
 		
 	}
 
