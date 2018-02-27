84cee78262c167132c4936707fdf9e0ff9ce8a92
FANG CAO <fc63@njit.edu>
2018-02-26 23:01:04 -0500

    sort working

--- mergesort/src/mergesort/MergeSort.java
+++ mergesort/src/mergesort/MergeSort.java
@@ -5,22 +5,17 @@ public class MergeSort {
 	private int[] myArray;
     private int[] helper;
 
-    public MergeSort(int[] a) {
-    	
-        this.myArray = a;      
-        this.helper = new int[a.length];
+    public MergeSort(int[] a) 
+    {
+        this.myArray = a;              
     }
 
     private void Sort(int leftIdx, int rightIdx) 
     {       
         if (leftIdx < rightIdx) 
-        {
-        	System.out.printf("leftIdx %d, rightIdx %d\n", leftIdx, rightIdx);
-        	PrintArray(myArray,leftIdx, rightIdx);
-        	
+        {        	
             // split the array from the middle
             int middle = leftIdx + (rightIdx - leftIdx) / 2;
-            System.out.printf("middle %d\n\n", middle);
             
             Sort(leftIdx, middle);
             Sort(middle + 1, rightIdx);
@@ -28,37 +23,39 @@ public class MergeSort {
             Merge(leftIdx, middle, rightIdx);
         }
     }
-
+        
     private void Merge(int leftIdx, int middle, int rightIdx) 
     {
-    	System.out.printf("leftIdx %d, middle %d, rightIdx %d\n",leftIdx, middle, rightIdx);
-        for (int i = leftIdx; i <= rightIdx; i++) {
-            helper[i] = myArray[i];
-        }
-
-        int l1 = leftIdx;	// helper array index
-        int m = middle + 1;	// beginning of right array
-        int l2 = leftIdx;	// myArray index
-       
-        // helper[] = { a,b,c ... | x,y,z ... }
+        // myArray[]= { ...   a,b,c ... | ... x,y,z ...}
+        // helper[] = 		{ a,b,c ... |     x,y,z ...}
+        //					  l			      r        
+    	        
+        int size1 = rightIdx - leftIdx + 1;
+        helper = new int[size1];    	
+        for (int i = 0; i < size1; i++)
+            helper[i] = myArray[leftIdx + i];
         
-        while (l1 <= middle && m <= rightIdx) {
-            if (helper[l1] <= helper[m]) {
-                myArray[l2] = helper[l1];
+        int l1 = leftIdx;	// helper array index
+        int r1 = middle + 1;	// beginning of right array
+        int l2 = leftIdx;	// myArray index
+               
+        while (l1 <= middle && r1 <= rightIdx) {
+            if (helper[l1-leftIdx] <= helper[r1-leftIdx]) {
+                myArray[l2] = helper[l1-leftIdx];
                 l1++;
             } else {
-                myArray[l2] = helper[m];
-                m++;
+                myArray[l2] = helper[r1-leftIdx];
+                r1++;
             }
             l2++;
         }
         // copy the rest
         while (l1 <= middle) {
-            myArray[l2] = helper[l1];
+            myArray[l2] = helper[l1-leftIdx];
             l2++;
             l1++;
         }
-    }
+    }   
     
 	public void PrintArray(int a[])
 	{
@@ -76,11 +73,12 @@ public class MergeSort {
 	
 	public static void main(String[] args)
 	{				
-		int array[] = {3,2,1,0};
+		int array[] = {7,6,5,4,3,2,1,0};
 		MergeSort ms = new MergeSort(array);
 		
 		ms.PrintArray(array);
 		ms.Sort(0, array.length - 1);
 		ms.PrintArray(array);
+		System.out.println("done");
 	}
 }
